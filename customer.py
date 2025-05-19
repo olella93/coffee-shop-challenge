e7d416076b588aeadbc6fdb0c78356a01d44c342
Richard <olella93@gmail.com>
2025-05-19 21:07:37 +0300

    customer.py updated with create_order

--- customer.py
+++ customer.py
@@ -15,8 +15,12 @@ class Customer:
         self._name = value
 
     def orders(self):
-        from order import Order  
+        from order import Order
         return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
         return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        from order import Order
+        return Order(self, coffee, price)
